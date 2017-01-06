cd7a49b3a349c1e03e6ba591c899d35587097049
Drew Manlove <andrew.manlove@codefutures.com>
2017-01-05 17:09:49 -0700

    save change

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -77,7 +77,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	}
 	fmt.Println("query did not find func: " + function)						//error
 
-	return nil, errors.New("Received unknown function query: " + function)
+	return nil, errors.New("ERROR: Received unknown function query: " + function)
 }
 
 func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
